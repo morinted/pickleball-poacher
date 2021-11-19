20cd222f5ebec681b5ef58b833351f45bbfd8a61
Ted Morin <morinted@gmail.com>
2021-11-19 17:39:16 -0500

    Setup environment, start puppeteer work

--- README.md
+++ README.md
@@ -4,11 +4,10 @@ City of Ottawa recreation centre drop-in reservation script.
 
 ## Why
 
-The City of Ottawa opens registration for drop-in events 2 days in advance at 6 PM. It can be hard to remember to sign up at that time and the site also suffers from traffic at that time making it slow. If you intend to sign up for multiple people or multiple slots, it can be a delecate balancing act. On top of that, if you try to fill out the form multiple times without refreshing the browser page, *it can fail silently, informing you that your reservation was successful when it wasn't.*
-
+The City of Ottawa opens registration for drop-in events 2 days in advance at 6 PM. It can be hard to remember to sign up at that time and the site also suffers from traffic at that time making it slow. If you intend to sign up for multiple people or multiple slots, it can be a delecate balancing act. On top of that, if you try to fill out the form multiple times without refreshing the browser page, _it can fail silently, informing you that your reservation was successful when it wasn't._
 
 ## How
 
 The goal is to enumerate the slots you'd like to reserve, and then run the script on a schedule where it will wake up once a day at 6 PM and fulfill any outstanding reservations. It should be able to do these in tandem and will retry if the site's connection is spotty. It won't stop until it finds a spot, so if the day fills up it will retry over time.
 
-It will need n sets of email + phone, which would match up to how many slots you want to reserve * the number of people you want to reserve for, divided by two. E.g. if you want to schedule for 2 people for 3 sessions at once, that is 3 identities. If you want to register for 6 people, 2 sessions at once, t'at is 6 identities.
+It will need n sets of email + phone, which would match up to how many slots you want to reserve \* the number of people you want to reserve for, divided by two. E.g. if you want to schedule for 2 people for 3 sessions at once, that is 3 identities. If you want to register for 6 people, 2 sessions at once, t'at is 6 identities.
--- index.js
+++ index.js
@@ -1,21 +1,120 @@
-// https://reservation.frontdesksuite.ca/rcfs/
-// hintonburgcc Wednesday Pickleball 6, 7:15, 8:30
-// francoisdupuis Pickleball - Adult Thursday 6:30, 7:45
-// francoisdupuis Pickleball - All ages Sunday 12:30, 1:45
-// mintobarrhaven Pickleball Saturday 11:45, 1:00
-// richcraftkanata Pickleball Sunday 10:15
+#!/usr/bin/env node
 
-function getElementWithText(selector, text, root = document) {
-    return [...root.querySelectorAll(selector).values()].find(el => el.textContent.includes(selector))
- }
- 
- getElementWithText('a', 'Pickleball')
- document.querySelector('input#reservationCount')
- const scheduleForDay = getElementWithText('.date', 'Wednesday')
- getElementWithText('a', '7:45', scheduleForDay) // Disabled: li.reserved
- document.querySelector('input#telephone')
- document.querySelector('input#email')
- const nameContainer = getElementWithText('label', 'Name')
- const nameInput = nameContainer.querySelector('input')
- document.querySelector('#submit-btn')
- 
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import Conf from 'conf'
+import moment from 'moment-timezone'
+import puppeteer from 'puppeteer'
+
+const getEventMoment = ({ day, time }) =>
+  moment.tz(time, ['h:m a', 'H:m'], 'America/Toronto').day(day)
+const formatEventMoment = (eventMoment) =>
+  eventMoment.format('YYYY/MM/DD h:mm a')
+
+yargs(hideBin(process.argv))
+  .command('init', 'create a default config file', () => {
+    const config = new Conf()
+    config.set('events', [
+      {
+        day: 'Wednesday',
+        location: 'hintonburgcc',
+        time: '6:00',
+        activity: 'Pickleball',
+        spots: 1,
+      },
+    ])
+    config.set('identities', [
+      {
+        uniqueEmail: 'email@example.com',
+        uniquePhone: '6131231234',
+        name: 'Your name',
+      },
+    ])
+    config.set('registrations', [])
+    console.log(`Config file created! Edit it at ${config.path}`)
+  })
+  .command(
+    'register [interval] [limit]',
+    'repeatedly try to register for events',
+    (yargs) => {
+      return yargs
+        .positional('interval', {
+          describe: 'the interval in seconds to retry when sessions are filled',
+          default: 300,
+        })
+        .positional('limit', {
+          describe: 'the time limit in hours to retry for',
+          default: 23.75,
+        })
+    },
+    (yargs) => {
+      const config = new Conf()
+      const register = async () => {
+        const events = config.get('events')
+        const identities = config.get('identities')
+        const registrations = config.get('registrations', [])
+
+        const now = moment.tz('America/Toronto')
+        const afterSix = now.hour() >= 18
+        const day = now.day()
+        const registerableDays = [
+          day,
+          day + 1,
+          ...(afterSix ? [day + 2] : []),
+        ].map((dayNumber) => moment().day(dayNumber).format('dddd'))
+
+        const targetEvents = events.filter(
+          ({ day, time, location, activity }) => {
+            const validDay = registerableDays.includes(day)
+            const eventMoment = getEventMoment({ day, time })
+            const date = formatEventMoment(eventMoment)
+            const eventAlreadyBooked = registrations.some(
+              (registration) =>
+                registration.date === date &&
+                registration.location === location &&
+                registration.activity === activity
+            )
+            return validDay && eventMoment.isAfter(now) && !eventAlreadyBooked
+          }
+        )
+
+        const browser = await puppeteer.launch()
+        const results = Promise.all(
+          targetEvents.map(async (targetEvent) => {
+            const page = await browser.newPage()
+            await page.goto(
+              `https://reservation.frontdesksuite.ca/rcfs/${targetEvent.location}`,
+              { waitUntil: 'networkidle0' }
+            )
+
+            const [button] = await page.$x(
+              `//button[contains(., '${targetEvent.activity}')]`
+            )
+            if (!button) {
+              return `Activity not found ${JSON.stringify(targetEvent)}`
+            }
+            await button.click()
+
+            function getElementWithText(selector, text, root = document) {
+              return [...root.querySelectorAll(selector).values()].find((el) =>
+                el.textContent.includes(selector)
+              )
+            }
+
+            getElementWithText('a', 'Pickleball')
+            document.querySelector('input#reservationCount')
+            const scheduleForDay = getElementWithText('.date', 'Wednesday')
+            getElementWithText('a', '7:45', scheduleForDay) // Disabled: li.reserved
+            document.querySelector('input#telephone')
+            document.querySelector('input#email')
+            const nameContainer = getElementWithText('label', 'Name')
+            const nameInput = nameContainer.querySelector('input')
+            document.querySelector('#submit-btn')
+
+            await browser.close()
+          })
+        )
+      }
+    }
+  )
+  .parse()
